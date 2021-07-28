47fd6c6afe3ac67815fbc1e49f933ee84fe936e5
Mohamed ElSaadany <mhsaadany@gmail.com>
2021-07-28 03:42:32 +0300

    prepare for pypi publishing

--- setup.py
+++ setup.py
@@ -1,25 +1,33 @@
-import setuptools
+from setuptools import setup
+import pathlib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setuptools.setup(
-    name="subscene-dl-sa3dany",
-    version="0.2",
-    author="Mohamed ElSaadany",
-    author_email="mhsaadany@gmail.com",
-    description="Download movie subtitles from subscene.com",
+setup(
+    name='subscene-dl',
+    version='0.3.0',
+    description='Download movie subtitles from subscene.com',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    keywords='subscene, subtitles, cli',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
     ],
-    python_requires=">=3.6",
-    install_requires=["colorama", "iso-639",
-                      "jellyfish", "lxml", "requests", "xextract"],
-    entry_points={"console_scripts": ["subscene-dl = subscene.cli:main"]},
+
+    python_requires='>=3.6',
+    packages=['subscene'],
+    install_requires=[
+        'colorama', 'iso-639', 'jellyfish', 'lxml', 'requests', 'xextract'
+    ],
+    extras_require={
+        'dev': ['build', 'sphinx', 'twine'],
+    },
+    entry_points={
+        'console_scripts': [
+            'subscene-dl=subscene.cli:main'
+        ]
+    },
 )
